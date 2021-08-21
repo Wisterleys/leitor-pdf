daf5102441942abea379ec36198d2b9aed03aa61
Wisterleys <wister.sousa2@gmail.com>
2021-08-21 14:01:14 -0300

    Importações e instalações npm realizadas

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const fs=require('fs');
 const PDFParser=require('pdf2json');
+const Express = require("express");
+const app = Express();
+const port = 8080; //porta
+
+app.get("/",(req,res)=>{
+    res.setHeader('content-type','application/json');
+    res.json({info:"OK rodando"})
+})
+
+app.listen(port,e=>console.log("app rodando na porta "+port))
