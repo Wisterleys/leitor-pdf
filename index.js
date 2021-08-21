5a183f796cd11bc5f94915653c7639ea888a08a4
Wisterleys <wister.sousa2@gmail.com>
2021-08-21 16:01:34 -0300

    config pasta readfile

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = 8080; //porta
 
 app.use(Express.static(path.join(__dirname, 'public')));
 
-function readPDF(pdfCaminho){
+function readPDF(pdfCaminho,res){
     if (fs.existsSync(pdfCaminho)) {
         var pdfParser = new PDFParser();
         pdfParser.on("pdfParser_dataError", function (errData) {
            console.error(errData.parserError)
         });
         pdfParser.on("pdfParser_dataReady", function (pdfData) {
-       console.log(pdfData)
+            res.json(pdfData)
         });
         pdfParser.loadPDF(pdfCaminho);
         console.log('Arquivo localizado');
@@ -22,14 +22,11 @@ function readPDF(pdfCaminho){
           console.log('Arquivo não localizado');
       }
 }
-app.get("/",(req,res)=>{
-    console.log(__dirname)
+
+app.get("/readpdf/:name",(req,res)=>{
     res.setHeader('content-type','application/json');
-    res.json({info:"OK rodando"})
-})
-app.get("/readpdf",(req,res)=>{
-    res.setHeader('content-type','application/json');
-    res.json({info:"OK rodando"})
+    readPDF(__dirname+`/public/readFiles/${req.params.name}`,res)
+    
 })
 
 app.listen(port,e=>console.log("app rodando na porta "+port))
